56cf47099f6fdd0d4e48e0c2bdc5a4ba87d31a7d
maede <maedemoeini78@gmail.com>
2022-11-25 13:00:54 +0330

    create App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Todo from "./component/Todo";
+
+function App() {
+  return (
+<div>
+  <h1>My Todos</h1>
+  <Todo text="Learn react"/>
+  <Todo text="Hi react"/>
+  <Todo text="ExampleOneReact"/>
+</div>
+
+  );
+}
+
+export default App;
